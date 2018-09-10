785999e87c8fd0f28d42b35481b932bbfa835deb
ezraschwepker@gmail.com <ezraschwepker@gmail.com>
2018-09-10 16:01:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var line = [];
 
 function takeANumber(length, name) {
   line.push(name);
-  return `Welcome, ${name}. You are number ${line.length + 1} in line.`;
+  return `Welcome, ${name}. You are number ${line.length} in line.`;
 }
 
 function nowServing(line) {
