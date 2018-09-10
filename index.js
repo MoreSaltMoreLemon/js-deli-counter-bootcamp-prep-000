324d5bfef5a3f544748a8abbf5968410d2acd8a9
ezraschwepker@gmail.com <ezraschwepker@gmail.com>
2018-09-10 16:03:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ function nowServing(line) {
   if (line.length == 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return line.pop()
+    return `Currently serving ${line.pop()}`;
   }
 }
 
 function currentLine (line) {
   let string = "The line is currently:";
   if (line.length == 0) {
-    return "The line is currently empty";
+    return "The line is currently empty.";
   } else {
     for (let i = 0; i < line.length; i++) {
       string = string + " "+ (i + 1) + ", " + line[i] + ".";
