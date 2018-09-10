6f2b0b100252767c1bcb683d675c50d115657515
ezraschwepker@gmail.com <ezraschwepker@gmail.com>
2018-09-10 16:04:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ function nowServing(line) {
   if (line.length == 0) {
     return "There is nobody waiting to be served!";
   } else {
-    return `Currently serving ${line.pop()}`;
+    let serving = line.shift()
+    return `Currently serving ${serving}`;
   }
 }
 
